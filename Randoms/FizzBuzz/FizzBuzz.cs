57a63f0f0bfb5c6cb20edeb27c4ad5a2d41e30ed
Vojislav Beli <94176223+Gloomcaller@users.noreply.github.com>
2024-04-03 11:01:48 +0200

    Delete Randoms directory

--- Randoms/FizzBuzz/FizzBuzz.cs
+++
@@ -1,7 +0,0 @@
-for (int i = 1; i < 101; i++){
-    if (i%3 == 0 && i%5 == 0) Console.Write(i + " FizzBuzz");
-    else if (i%5 == 0) Console.Write(i + " Buzz");
-    else if (i%3 == 0) Console.Write(i + " Fizz");
-    else Console.Write(i);
-    Console.WriteLine("");
-}
